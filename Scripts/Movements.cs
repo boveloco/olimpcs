85d3437e92b7a4d3889c2f1e2d9f331b4b1d825b
André Bovendorp <Andr Bovendorp>
2016-08-25 03:21:52 -0300

    First scripts

---
+++ Scripts/Movements.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movements : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+}
+
---
+++ Scripts/Turn/TurnController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TurnController : MonoBehaviour {
+
+
+    public Text timer;
+    private GameObject[] objects;
+
+    private int lastTurn;
+    private int turn = 0;
+
+    private float timeLeft = 30;
+
+	// Use this for initialization
+	void Start () {
+
+        objects = GameObject.FindGameObjectsWithTag("Player");
+        lastTurn = objects.Length;
+
+        foreach (GameObject o in objects)
+        {
+            o.GetComponent<Movements>().enabled = false;
+        }
+
+        changeTurn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            changeTurn();
+        }
+
+        setTimer();
+	    }
+
+    private GameObject toogleObjectTransform(GameObject objecto, bool activate)
+    {
+        objecto.GetComponent<Movements>().enabled = activate;
+        return objects[turn];
+    }
+
+    private TurnController controlObjects()
+    {
+        lastTurn = turn;
+
+        if (objects.Length - 1 > turn)
+            turn++;
+        else
+            turn = 0;
+
+        return this;
+    }
+
+    private void setTimer()
+    {
+        if (timeLeft < 0)
+        {
+            timeLeft = 30;
+            changeTurn();
+        }
+        else
+            timeLeft -= Time.deltaTime;
+
+        timer.text = timeLeft.ToString();
+    }
+
+    private void changeTurn()
+    {
+        controlObjects();
+        toogleObjectTransform(objects[lastTurn], false);
+        toogleObjectTransform(objects[turn], true);
+    }
+
+}
